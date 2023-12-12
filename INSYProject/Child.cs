0c90f8597682f4f23f702857d2a3137cc81dca08
Jamie2002 <jamie.goliday@marquette.edu>
2023-12-12 16:19:15 -0600

    Jamie's project

---
+++ INSYProject/Child.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSYProject
+{
+    internal class Child
+    {
+        private string _name;
+        private string _personInCharge;
+        private double _age;
+        private string _phoneNumber;
+        private int _childID;
+
+
+        public Child(string name, string personInCharge, double age, string phoneNumber, int childID)
+        {
+            _name = name;
+            _personInCharge = personInCharge;
+            _age = age;
+            _phoneNumber = phoneNumber;
+            _childID = childID;
+        }
+
+
+    }
+}
